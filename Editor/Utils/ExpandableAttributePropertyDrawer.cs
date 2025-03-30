55d92afffb618851ed2d29e16c3ce4bbc52619c1
Ian Sinclair <ian.g.sinclair@gmail.com>
2025-03-30 16:43:04 +0200

    animating expandable property

--- Editor/Utils/ExpandableAttributePropertyDrawer.cs
+++ Editor/Utils/ExpandableAttributePropertyDrawer.cs
@@ -1,28 +1,47 @@
 using UnityEditor;
+using UnityEditor.AnimatedValues;
 using UnityEngine;
 
 [CustomPropertyDrawer(typeof(ExpandableAttribute))]
 public class ExpandableAttributePropertyDrawer : PropertyDrawer
 {
-    Editor _editor = null;
+    private Editor _editor = null;
+
+    private AnimBool _animBool;
 
     public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
     {
+        EditorGUI.BeginProperty(position, label, property);
+        
         EditorGUI.PropertyField(position, property, label, true);
         if (property.objectReferenceValue == null)
             return;
 
-        if (property.isExpanded = EditorGUI.Foldout(position, property.isExpanded, GUIContent.none))
+        if (_animBool == null)
         {
-            EditorGUI.indentLevel++;
-            if(!_editor)
-                Editor.CreateCachedEditor(property.objectReferenceValue, null, ref _editor);
-            _editor.OnInspectorGUI();
-            EditorGUI.indentLevel--;
-            // using (new EditorGUILayout.VerticalScope(EditorStyles.helpBox))
-            // {
-            // }
-            // using (new EditorGUI.IndentLevelScope()){}
+            _animBool = new AnimBool(property.isExpanded);
+            _animBool.valueChanged.AddListener(() =>
+            {
+                EditorWindow.focusedWindow.Repaint();
+            });
         }
+
+        property.isExpanded = EditorGUI.Foldout(position, property.isExpanded, GUIContent.none);
+        _animBool.target = property.isExpanded;
+
+        if (EditorGUILayout.BeginFadeGroup(_animBool.faded))
+        {
+            using (new GUILayout.VerticalScope(EditorStyles.helpBox))
+            {
+                EditorGUI.indentLevel++;
+                if(!_editor)
+                    Editor.CreateCachedEditor(property.objectReferenceValue, null, ref _editor);
+                _editor.OnInspectorGUI();
+                EditorGUI.indentLevel--;
+            }
+        }
+        EditorGUILayout.EndFadeGroup();
+        
+        EditorGUI.EndProperty();
     }
 }
