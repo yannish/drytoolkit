313c451840e37c1810c970d80e289847ac4fdad4
Ian Sinclair <ian.g.sinclair@gmail.com>
2025-08-06 09:25:39 +0200

    removing PlasticGUI include

--- Editor/NestedAnimation/RuntimeRigEditor.cs
+++ Editor/NestedAnimation/RuntimeRigEditor.cs
@@ -2,7 +2,6 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
-using PlasticGui;
 using UnityEditor;
 using UnityEditor.Compilation;
 using UnityEditor.UIElements;
