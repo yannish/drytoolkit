b8eb4663c23f8e18662c33b2bb6bed8107109d18
yannish <ian.g.sinclair@gmail.com>
2025-09-12 16:17:28 +0200

    fixing viewOnly

--- Editor/Utils/ViewOnlyPropertyDrawer.cs
+++ Editor/Utils/ViewOnlyPropertyDrawer.cs
@@ -1,7 +1,17 @@
+using drytoolkit.Runtime.Utils;
+using UnityEditor;
+using UnityEngine;
+
 namespace drytoolkit.Editor.Utils
 {
-    public class ViewOnlyPropertyDrawer
+    [CustomPropertyDrawer(typeof(ViewOnly))]
+    public class ViewOnlyPropertyDrawer : PropertyDrawer
     {
-        
+        public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+        {
+            GUI.enabled = false;
+            base.OnGUI(position, property, label);
+            GUI.enabled = true;
+        }
     }
 }
