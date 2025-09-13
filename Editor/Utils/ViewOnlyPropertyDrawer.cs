203038373c91c31641631eca23aada345d1f21d4
Ian Sinclair <ian.g.sinclair@gmail.com>
2025-09-13 11:30:29 +0200

    tweak to ViewOnly

--- Editor/Utils/ViewOnlyPropertyDrawer.cs
+++ Editor/Utils/ViewOnlyPropertyDrawer.cs
@@ -1,7 +1,18 @@
+using drytoolkit.Runtime.Utils;
+using UnityEditor;
+using UnityEngine;
+
 namespace drytoolkit.Editor.Utils
 {
-    public class ViewOnlyPropertyDrawer
+    [CustomPropertyDrawer(typeof(ViewOnlyAttribute))]
+    public class ViewOnlyPropertyDrawer : PropertyDrawer
     {
-        
+        public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+        {
+            bool wasEnabled = GUI.enabled;
+            GUI.enabled = false;
+            EditorGUI.PropertyField(rect, prop, label);
+            GUI.enabled = wasEnabled;
+        }
     }
 }
