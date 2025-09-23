ed831359e656689f65b6493a8f2bc0cc54e2f66f
Ian Sinclair <ian.g.sinclair@gmail.com>
2025-09-23 19:48:27 +0200

    quick switch around for cleaning runtime sets

--- Editor/Arch/RuntimeSetUtils.cs
+++ Editor/Arch/RuntimeSetUtils.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 using UnityEditor;
 
@@ -6,21 +7,38 @@ public class RuntimeSetUtils
     [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.SubsystemRegistration)]
     public static void Cleanup()
     {
+        Debug.LogWarning("Registering subsystem cleanup for runtime sets.");
         EditorApplication.playModeStateChanged -= ClearRuntimeSets;
         EditorApplication.playModeStateChanged += ClearRuntimeSets;
     }
 
     public static void ClearRuntimeSets(PlayModeStateChange state)
     {
-        if (state != PlayModeStateChange.ExitingPlayMode)
-            return;
-        
-        Debug.LogWarning("Cleaning up runtime sets;");
-
-        EditorApplication.delayCall += () =>
+        switch (state)
         {
-            Debug.LogWarning("Cleaning up runtime sets;");
+            case PlayModeStateChange.EnteredEditMode:
+                CleanUpRuntimeSets();
+                break;
+            case PlayModeStateChange.ExitingEditMode:
+                CleanUpRuntimeSets();
+                break;
+            case PlayModeStateChange.EnteredPlayMode:
+                break;
+            case PlayModeStateChange.ExitingPlayMode:
+                break;
+            default:
+                throw new ArgumentOutOfRangeException(nameof(state), state, null);
+        }
 
+        // EditorApplication.delayCall += () =>
+        // {
+        //     Debug.LogWarning("Cleaning up runtime sets;");
+        //
+        //     CleanUpRuntimeSets();
+        // };
+
+        void CleanUpRuntimeSets()
+        {
             var allRuntimeSetTypes = TypeCache.GetTypesDerivedFrom(typeof(RuntimeSet<>));
             foreach (var runtimeSetType in allRuntimeSetTypes)
             {
@@ -34,6 +52,6 @@ public class RuntimeSetUtils
                     }
                 }
             }
-        };
+        }
     }
 }
